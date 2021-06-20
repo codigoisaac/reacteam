81edf76331ad4d97531e095b99f8914748e0245c
Isaac Muniz <codigoisaac@protonmail.com>
2021-06-20 20:19:15 -0300

    Move App component to its own file

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, React! Now for fucking real!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello, React! Now for fucking real!</h1>
-      </div>
-    );
-  }
-}
 
 ReactDOM.render(<App />, document.getElementById("root"));
